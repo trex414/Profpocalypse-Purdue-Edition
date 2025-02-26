d44d9f05a89190445fd350b802d12f3314bff183
Austin Rogers <rqq@Austins-MacBook-Pro-3.local>
2025-02-26 14:59:58 -0500

    update key bindings menu with close button

--- Settings/scenes/control_settings.gd
+++ Settings/scenes/control_settings.gd
@@ -4,17 +4,21 @@ extends Control
 @onready var delete_button = $"Panel/VBoxContainer/Delete Button"
 @onready var close_button = $"Panel/VBoxContainer/Close Menu"
 @onready var keybind_menu_button = $"Panel/VBoxContainer/Key Binds Button"
-@onready var keybind_menu = $"Panel/VBoxContainer/Key Binds Button/Control-Key Binds"
+@onready var keybind_menu = $"Panel/VBoxContainer/Control - Key Bind Menu"
+@onready var close_keybind_button = $"Panel/VBoxContainer/Control - Key Bind Menu/Panel/VBoxContainer/Close Keybind Menu"
 
 var is_open = false
+var keybind_menu_open = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
+	keybind_menu.visible = false
 	save_button.pressed.connect(_on_save)
 	delete_button.pressed.connect(_on_delete)
 	close_button.pressed.connect(toggle_menu)
 	keybind_menu_button.pressed.connect(toggle_keybind_menu)
+	close_keybind_button.pressed.connect(_on_CloseMenuButton_pressed)
 
 
 
@@ -27,6 +31,10 @@ func toggle_menu():
 	is_open = !is_open
 	visible = is_open
 	#get_tree().paused = is_open    # ADD THIS TO PAUSE GAME WHEN SETTINGS IS OPEN
+	
+	if not is_open:
+		keybind_menu.visible = false
+		keybind_menu_open = false
 
 func _on_save():
 	SaveManager.save()
@@ -35,8 +43,10 @@ func _on_delete():
 	SaveManager.delete()
 	
 func toggle_keybind_menu():
-	keybind_menu.visible = true
+	keybind_menu_open = !keybind_menu_open
+	keybind_menu.visible = keybind_menu_open
 	
 func _on_CloseMenuButton_pressed():
 	keybind_menu.visible = false
+	keybind_menu_open = !keybind_menu_open
 	
