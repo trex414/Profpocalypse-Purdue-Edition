db80c59e5eaca131e1dd8e33b3bccc00903b5204
Austin Rogers <rqq@Austins-MacBook-Pro-3.local>
2025-04-25 19:03:03 -0400

    changes to auditorium area

--- BuildingInteriors/ELLTHall/Scenes/ellt.gd
+++ BuildingInteriors/ELLTHall/Scenes/ellt.gd
@@ -3,6 +3,8 @@ extends Node2D
 var entered = false
 var entered2 = true
 var graduated = false
+var scene_ready = false
+var has_entered_area2d2 = false
 @onready var fade_rect = $ColorRect
 @onready var exitpop = $UI/Control
 @onready var exitpoplabel = $UI/Control/Panel/Label
@@ -18,6 +20,7 @@ var graduated = false
 @onready var control = $UI2/Control
 
 func _ready():
+	control.visible = false
 	streamer.visible = false
 	streamer1.visible = false
 	streamer2.visible = false
@@ -29,10 +32,11 @@ func _ready():
 
 	yes_button.pressed.connect(_on_yes_pressed)
 	no_button.pressed.connect(_on_no_pressed)
-	control.visible = false
+	await get_tree().create_timer(0.1).timeout  # Wait a tiny bit (0.1 seconds)
+	scene_ready = true  # Now allow signals to trigger actions
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if entered:
+	if entered && !graduated:
 		exitpoplabel.text = ("Do you wish to exit?")
 		exitpop.show()
 
@@ -41,8 +45,19 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	
 	
 
+#func _on_area_2d2_body_entered(body: Node2D) -> void:
+#	if scene_ready && entered2 && !graduated:
+#		control.visible = true
+
 func _on_area_2d2_body_entered(body: Node2D) -> void:
-	if entered2 && !graduated:
+	if not scene_ready:
+		return  # ignore anything during scene load
+
+	if !has_entered_area2d2:
+		has_entered_area2d2 = true  # First time entering, mark it
+		return  # IMPORTANT: Ignore the very first signal after spawn
+
+	if entered2 and !graduated:
 		control.visible = true
 
 func _on_area_2d2_body_exited(body: Node2D) -> void:
@@ -77,5 +92,11 @@ func fade_and_do_something():
 	streamer3.visible = true
 	streamer4.visible = true
 	music.play()
-	music.play()
 	fade_in()
+	await get_tree().create_timer(8).timeout
+	music.play()
+	await get_tree().create_timer(8).timeout
+	fade_out()
+	await get_tree().create_timer(2).timeout
+	$TemporaryPlayer.visible = false
+	$TextureRect.visible = true
